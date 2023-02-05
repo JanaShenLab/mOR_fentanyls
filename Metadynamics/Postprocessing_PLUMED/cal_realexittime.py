c6f8dfb3c72867e92d313b00d89ca7baed8e4295
mahinth1 <113048114+mahinth1@users.noreply.github.com>
2023-02-04 22:23:35 -0500

    Add files via upload

---
+++ Metadynamics/Postprocessing_PLUMED/cal_realexittime.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#read file obtained from PLUMED
+#CV1 is z-position (used as cutoff for exit)
+data = pd.read_csv('pmfxy/DZ',delimiter='\s+',comment='#',
+        header=None,names=['time','CV1','CV2','bias','rct','acc']) 
+
+#z = 15 angstrom from COM of binding pocket as exit cutoff
+data = data[:data[data['CV1']>15].index[0]+1]
+
+x = data['time'].tolist()
+y = data['acc'].tolist()
+print('real exit time (sec): ', x[-1]*y[-1]/10**11)
+
